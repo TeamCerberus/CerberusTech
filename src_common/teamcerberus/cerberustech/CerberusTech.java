58a12de5474983af720d7d9f37ebe2afc02cac01
Chandler Newman <chandler2neewman@aol.co.uk>
2013-05-08 18:07:40 +0100

    Configuration Work

--- src_common/teamcerberus/cerberustech/CerberusTech.java
+++ src_common/teamcerberus/cerberustech/CerberusTech.java
@@ -1,6 +1,16 @@
 package teamcerberus.cerberustech;
 
+import net.minecraft.item.EnumArmorMaterial;
+import net.minecraft.item.ItemArmor;
+import net.minecraftforge.common.Configuration;
+import teamcerberus.cerberuscore.config.ConfigurationOption;
+import teamcerberus.cerberuscore.config.ConfigurationParser;
+import teamcerberus.cerberuscore.config.ItemID;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = CerberusTech.id, name = CerberusTech.id,
@@ -9,4 +19,14 @@ import cpw.mods.fml.common.network.NetworkMod;
 public class CerberusTech {
 	public final static String	id		= "CerberusTech";
 	public final static String	version	= "@VERSION@";
+	
+	
+	@PreInit
+	public void preinit(FMLPreInitializationEvent e){
+		ConfigurationParser.Parse(this, new Configuration(e.getSuggestedConfigurationFile()));
+	}
+	
+	@Init
+	public void init(FMLInitializationEvent e){
+	}
 }
