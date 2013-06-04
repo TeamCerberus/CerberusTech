542c35a9dc218a193417038b841f27a177cd6849
Aroma1997 <robin.marius@web.de>
2013-06-04 15:16:53 +0200

    Added ItemBlock for Generators.

---
+++ src_common/teamcerberus/cerberustech/energy/generators/ItemBlockGenerator.java
@@ -0,0 +1,17 @@
+package teamcerberus.cerberustech.energy.generators;
+
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+
+public class ItemBlockGenerator extends ItemBlock {
+
+	public ItemBlockGenerator(int id) {
+		super(id);
+	}
+	
+	@Override
+	public String getUnlocalizedName(ItemStack itemstack) {
+		return GeneratorType.values()[itemstack.getItemDamage()].getLocalizedName();
+	}
+
+}
