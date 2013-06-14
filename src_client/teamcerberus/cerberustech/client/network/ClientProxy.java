5eac72406f5d8c10f92eaea994a9b2d95c9b0399
Chandler Newman <chandler2neewman@aol.co.uk>
2013-06-14 23:54:27 +0100

    Multiblock testing

--- src_client/teamcerberus/cerberustech/client/network/ClientProxy.java
+++ src_client/teamcerberus/cerberustech/client/network/ClientProxy.java
@@ -1,7 +1,13 @@
 package teamcerberus.cerberustech.client.network;
 
+import teamcerberus.cerberuscore.multiblock.MultiblockManager;
+import teamcerberus.cerberuscore.render.CerbRenderManager;
 import teamcerberus.cerberustech.network.CommonProxy;
 
 public class ClientProxy extends CommonProxy {
-
+	@Override
+	public void clientSetup() {
+		CerbRenderManager.init();
+		MultiblockManager.clientInit();
+	}
 }
